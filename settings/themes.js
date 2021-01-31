7ca61c1e9c99ef5530fa64899d8873ec1b6b8e09
jamesstudd <james.studd@live.co.uk>
2021-01-31 17:31:03 +0000

    Added blue theme

--- settings/themes.js
+++ settings/themes.js
@@ -10,7 +10,6 @@ export default [
 			deselected: "#E5989B",
 			shadow: "#613f40",
 			selected: "#fff",
-			border: "#B5838D",
 		},
 		particle: {
 			background: "#FFB4A2",
@@ -23,17 +22,34 @@ export default [
 		background: "#2E5339",
 		settings: {
 			background: "#E9EDC9",
-			border: "798478",
+			border: "#798478",
 		},
 		note: {
 			deselected: "#E9EDC9",
 			shadow: "#7d806b",
 			selected: "#798478",
-			border: "#CCD5AE",
 		},
 		particle: {
 			background: "#666039",
 			border: "#FAEDCD",
+		},
+		text: "#000",
+	},
+	{
+		name: "Blue",
+		background: "#12292C",
+		settings: {
+			background: "#A7A5C6",
+			border: "#4f4e5e",
+		},
+		note: {
+			deselected: "#A7A5C6",
+			shadow: "#4f4e5e",
+			selected: "#fff",
+		},
+		particle: {
+			background: "##c1bfe0",
+			border: "#a8a6bf",
 		},
 		text: "#000",
 	},
