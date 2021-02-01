7dc83345aa0197112fa96f577711e28e13a118fe
jamesstudd <james.studd@live.co.uk>
2021-02-01 18:57:06 +0000

    changeeeeeees

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.body.style.setProperty("--vh", `${vh}px`);
-
 let isClosed = false;
 
 const USING_MOBILE = mobileCheck();
